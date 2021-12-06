24925babc012b70f55323179db0aff9d62ee5358
Tikpeme <tete.ikpeme@gmail.com>
2021-12-06 00:53:28 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,89 @@
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3]
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9]
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
+
+// An array of all the arrays above
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
+
+
+// Add your functions below:
+const validateCred = (arr) =>{
+    let checkArray = arr.map((x) => x);
+
+    for(let i = arr.length-2; i >= 0; i =i-2){
+        /*if(i === arr.length-1){ //ignore the check digit
+            console.log(`made it to check digit. Now at index: ${i}`)
+            continue
+        }*/
+        checkArray[i] = checkArray[i] * 2
+        if(checkArray[i]>9) checkArray[i] = checkArray[i] - 9 //if greater than 9 affter doubling subtract 9
+        //console.log(checkArray)
+    
+    }
+    let arraySum = checkArray.reduce( (previousValue, currentValue) => previousValue+currentValue) 
+    //console.log(arraySum)
+    if(arraySum%10 === 0 ) return true
+    else 
+        return false
+
+}
+
+
+const findInvalidCards = (NestedArray) =>{
+    let invalidatedCards = NestedArray.filter(arr => validateCred(arr) === false )
+    //console.log(invalidatedCards)
+    //console.log(validateCred)
+   return invalidatedCards
+
+}
+
+const idInvalidCardCompanies = (NestedArray) =>{
+    let companies = []
+    NestedArray.forEach(arr =>{
+        if(arr[0] === 3 && !companies.includes('Amex')){    
+            companies.push('Amex')
+            console.log('Amex was added')
+        return
+    }
+        if(arr[0] === 4 && !companies.includes('Visa')){
+            companies.push('Visa')
+            console.log('Visa was added')
+        return
+        }
+        if(arr[0] === 5 && !companies.includes('Mastercard')) {
+            companies.push('Mastercard')
+            console.log('Mastercard was added')
+        return
+        }
+        if(arr[0] === 6 && !companies.includes('Discover')){
+            companies.push('Discover')
+            console.log('Discover was added')
+        return
+        }
+        if(arr[0]!==3 && arr[0]!==4 && arr[0]!==5 && arr[0]!==6 )
+            console.log('Company not found')
+    })
+    return companies
+}
+//console.log(findInvalidCards(batch).forEach(arr => console.log(validateCred(arr))))
+//findInvalidCards(batch)
+//console.log(validateCred(valid3))
+console.log(idInvalidCardCompanies(findInvalidCards(batch)))
+//console.log(`There are ${idInvalidCardCompanies(findInvalidCards(batch)).length} companies`)
